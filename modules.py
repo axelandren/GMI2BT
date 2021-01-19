7399733845f2f8195be35e04bb601ae7b85366af
Axel Andrén <76403117+axelandren@users.noreply.github.com>
2021-01-19 19:26:54 +0100

    Add files via upload

--- modules.py
+++ modules.py
@@ -6,6 +6,7 @@ def check_divisible(first, second):
             numbers_list.append(i)
         i = i + 1
     print(f'Result: {numbers_list}')
+    print(f'Average value is: {sum(numbers_list) / len(numbers_list)}')
     input('\nPress Enter to continue...')
 
 def guess_randint():
@@ -14,13 +15,13 @@ def guess_randint():
     amount_of_guesses = 0
     hit_the_target = False
     while hit_the_target == False:
-        amount_of_guesses = amount_of_guesses + 1
         while True:
             try:
                 guess = int(input('Guess the number: '))
                 break
             except ValueError:
                 print('Numbers only please...')
+        amount_of_guesses = amount_of_guesses + 1
         if guess == target:
             print(f'\nCongratulations! Number {guess} was the right answer!')
             print(f'It took you {amount_of_guesses} to get it right!')
