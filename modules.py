a6eef28475a51919a13819309907684e932b52ca
Axel Andrén <76403117+axelandren@users.noreply.github.com>
2021-01-20 10:49:33 +0100

    Add files via upload

--- modules.py
+++ modules.py
@@ -1,31 +1,35 @@
+def get_number():
+    while True:
+        try:
+            number = int(input())
+            break
+        except ValueError:
+            print('Numbers only please...')
+    return number
+
 def check_divisibility(first, second):
     numbers = []
-    i = 1
-    while i <= 1000:
-        if i % int(first) == 0 and i % int(second) == 0:
+    for i in range(1, 1001):
+        if i % first == 0 and i % second == 0:
             numbers.append(i)
-        i += 1
-    print(f'Result: {numbers}')
-    print(f'Average value is: {sum(numbers) / len(numbers)}')
-    input('\nPress Enter to continue...')
+    numbers_dict = {
+        'Result': numbers,
+        'Average': sum(numbers) / len(numbers)
+    }
+    return numbers_dict
 
 def guess_randint():
     from random import randint
     target = randint(1, 101)
     amount_of_guesses = 0
-    hit_the_target = False
-    while hit_the_target == False:
-        while True:
-            try:
-                guess = int(input('Guess the number: '))
-                break
-            except ValueError:
-                print('Numbers only please...')
+    print('Guess a number between 1 - 100')
+    while True:
+        guess = get_number()
         amount_of_guesses += 1
         if guess == target:
             print(f'\nCongratulations! {guess} was the right answer!\nIt took you {amount_of_guesses} tries to get it right.')
             input('\nPress Enter to continue...')
-            hit_the_target = True
+            break
         elif guess < target:
             print(f'The target number is higher than {guess}')
         elif guess > target:
--- program.py
+++ program.py
@@ -1,31 +1,22 @@
 import modules
 
-def get_number():
-    while True:
-        try:
-            number = int(input())
-            break
-        except ValueError:
-            print('Numbers only please...')
-    return number
-
-show_menu = True
-while show_menu:
+while True:
     print('\n+-----------------------------------+')
     print(' 1. Divisible numbers\n 2. Guess a number between 1 and 100\n 3. Exit program')
     print('+-----------------------------------+')
     choice = input('\nChoose an option: ')
     if choice == '1':
         print('Enter your first number')
-        first_number = get_number()
+        first_number = modules.get_number()
         print('Enter your second number')
-        second_number = get_number()
-        modules.check_divisibility(first_number, second_number)
+        second_number = modules.get_number()
+        numbers_dict = modules.check_divisibility(first_number, second_number)
+        print(numbers_dict)
+        input('\nPress Enter to continue...')
     elif choice == '2':
         modules.guess_randint()
     elif choice == '3':
         print('Exiting...')
-        show_menu = False
         break
     else:
         print('Type a valid option')
